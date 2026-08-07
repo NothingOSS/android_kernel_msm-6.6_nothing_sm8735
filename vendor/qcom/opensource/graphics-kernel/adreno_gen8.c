f5a1244b1e8d9a22885a77bc1c8ed3c49a453546
anderson.chen <anderson.chen@nothing.tech>
2026-08-07 15:51:08 +0800

    kgsl: Add buffer overflow check for perfcounter dynamic list
    Change-Id: Ib46926cde3a69503516c2e6e36bd76d5c87e0bb8

--- vendor/qcom/opensource/graphics-kernel/adreno_gen8.c
+++ vendor/qcom/opensource/graphics-kernel/adreno_gen8.c
@@ -2599,11 +2599,19 @@ int gen8_perfcounter_update(struct adreno_device *adreno_dev,
 	u16 perfcntr_list_len = lock->dynamic_list_len - gen8_dev->ext_pwrup_list_len;
 	unsigned long irq_flags;
 	int ret = 0;
+	u32 num_dependencies = 0;
+	u32 pending_triplets = 2;
 
 	if (!ADRENO_ACQUIRE_CP_SEMAPHORE(adreno_dev, irq_flags))
 		return -EBUSY;
 
 	if (flags & ADRENO_PERFCOUNTER_GROUP_RESTORE) {
+		for (i = 0; i < PERFCOUNTER_REG_DEPENDENCY_LEN && reg->reg_dependency[i]; i++)
+			num_dependencies++;
+
+		/* Number of triplets to add: 1 main + dependencies + 2 controls */
+		pending_triplets += num_dependencies + 1;
+
 		for (i = 0; i < perfcntr_list_len - 2; i++) {
 			if ((data[offset + 1] == reg->select) && (data[offset] == pipe)) {
 				start_offset = offset;
@@ -2614,6 +2622,13 @@ int gen8_perfcounter_update(struct adreno_device *adreno_dev,
 		}
 	} else if (perfcntr_list_len) {
 		goto update;
+	}
+
+	/* Ensure there is enough space in the reglist buffer for new triplets */
+	if ((start_offset == -1) && (offset + (pending_triplets * 3)) >=
+		(adreno_dev->pwrup_reglist->size / sizeof(u32))) {
+		ret = -ENOSPC;
+		goto err;
 	}
 
 	if (kgsl_hwlock(lock)) {
